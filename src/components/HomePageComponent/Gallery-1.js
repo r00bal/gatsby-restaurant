7d7399cc3030d326d9908a39ba4371b717c38e19
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-03-26 07:05:41 +0100

    add gallery

--- src/components/HomePageComponent/Gallery-1.js
+++ src/components/HomePageComponent/Gallery-1.js
@@ -4,26 +4,16 @@ import { styles, Section } from '../../utils'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-const SINGLE_IMAGE = graphql`
+const GET_IMAGES = graphql`
   {
-    img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    img2: file(relativePath: { eq: "homeGallery/img-2.jpeg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-    img3: file(relativePath: { eq: "homeGallery/img-3.jpeg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid_tracedSVG
+    getImages: allFile(filter: { relativeDirectory: { eq: "homeGallery" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
         }
       }
     }
@@ -33,26 +23,18 @@ const SINGLE_IMAGE = graphql`
 export default function Gallery() {
   return (
     <StaticQuery
-      query={SINGLE_IMAGE}
+      query={GET_IMAGES}
       render={data => {
-        const img1 = data.img1.childImageSharp.fluid
-        const img2 = data.img2.childImageSharp.fluid
-        const img3 = data.img3.childImageSharp.fluid
+        const images = data.getImages.edges
         return (
           <Section>
             <GalleryWrapper>
-              <div className="item item-1">
-                <Img fluid={img1} />
-                <p className="info"> awesome pizza</p>
-              </div>
-              <div className="item item-2">
-                <Img fluid={img2} />
-                <p className="info"> awesome pork</p>
-              </div>
-              <div className="item item-3">
-                <Img fluid={img3} />
-                <p className="info"> awesome stake</p>
-              </div>
+              {images.map(({ node }, index) => (
+                <div key={index} className={`item item-${index + 1}`}>
+                  <Img fluid={node.childImageSharp.fluid} />
+                  <p className="info">awesome pizza</p>
+                </div>
+              ))}
             </GalleryWrapper>
           </Section>
         )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import Quickinfo from '../components/HomePageComponent/QuickInfo'
+import Gallery from '../components/HomePageComponent/Gallery-1'
 import img from '../images/bcg/homeBcg.jpeg'
 
 const IndexPage = () => (
@@ -14,6 +15,7 @@ const IndexPage = () => (
       </Banner>
     </HomeHeader>
     <Quickinfo />
+    <Gallery />
   </Layout>
 )
 
